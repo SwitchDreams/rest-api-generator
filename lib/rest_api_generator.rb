83a731e70f8369eeae2ea80afb660a138ec43cd6
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-05-05 13:38:45 -0300

    :tada: Initialize gem

---
+++ lib/rest_api_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rest_api_generator/version"
+
+module RestApiGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rest_api_generator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RestApiGenerator
+  VERSION = "0.1.0"
+end
