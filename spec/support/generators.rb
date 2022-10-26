829fa3900d4ec0c230255af4eb7ecffc70a6a1f1
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-10-26 15:52:28 -0300

    Config for ammeter
    Co-authored-by: Waliff Cordeiro <waliffcordeiro@gmail.com>

---
+++ spec/support/generators.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module RestApiGenerator
+  module Specs
+    module Generators
+      module Macros
+        # Tell the generator where to put its output (what it thinks of as
+        # Rails.root)
+        def set_default_destination
+          destination DEFAULT_DESTINATION_PATH
+        end
+
+        def setup_default_destination
+          set_default_destination
+          before do
+            prepare_destination
+            # Create routes file
+            FileUtils.mkdir_p("#{DEFAULT_DESTINATION_PATH}/config")
+            FileUtils.touch("#{DEFAULT_DESTINATION_PATH}/config/routes.rb")
+          end
+        end
+      end
+
+      def self.included(klass)
+        klass.extend(Macros)
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include RestApiGenerator::Specs::Generators, type: :generator
+end
