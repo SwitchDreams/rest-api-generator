80eefadd7cd47dcdd774d840fd507b3ab811ba36
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-12-28 11:26:29 -0300

    Testing with new eject option

--- spec/lib/generators/rest_api_generator/resource_generator_spec.rb
+++ spec/lib/generators/rest_api_generator/resource_generator_spec.rb
@@ -7,41 +7,57 @@ require "support/generators"
 RSpec.describe RestApiGenerator::ResourceGenerator, type: :generator do
   setup_default_destination
 
-  before do
-    run_generator ["user"]
+  context "when run with normal attributes" do
+    before do
+      run_generator ["user"]
+    end
+
+    describe "controller file" do
+      subject { file("app/controllers/users_controller.rb") }
+
+      it { is_expected.to contain(/RestApiGenerator::ResourceController/) }
+      it { is_expected.to exist }
+    end
+
+    describe "spec for the controller" do
+      subject { file("spec/requests/users_controller_spec.rb") }
+
+      it { is_expected.to exist }
+    end
+
+    describe "model file" do
+      subject { file("app/models/user.rb") }
+
+      it { is_expected.to exist }
+    end
+
+    describe "migration file" do
+      subject { migration_file("db/migrate/create_users.rb") }
+
+      it { is_expected.to exist }
+    end
+
+    describe "routes" do
+      subject(:route_file) { file("config/routes.rb") }
+
+      it { is_expected.to exist }
+
+      it "has routes defined to the resource" do
+        expect(route_file).to contain("resources :users")
+      end
+    end
   end
 
-  describe "controller file" do
-    subject { file("app/controllers/users_controller.rb") }
+  context "when run with eject" do
+    before do
+      run_generator(["user", "--eject", "true"])
+    end
 
-    it { is_expected.to exist }
-  end
+    describe "controller file" do
+      subject { file("app/controllers/users_controller.rb") }
 
-  describe "spec for the controller" do
-    subject { file("spec/requests/users_controller_spec.rb") }
-
-    it { is_expected.to exist }
-  end
-
-  describe "model file" do
-    subject { file("app/models/user.rb") }
-
-    it { is_expected.to exist }
-  end
-
-  describe "migration file" do
-    subject { migration_file("db/migrate/create_users.rb") }
-
-    it { is_expected.to exist }
-  end
-
-  describe "routes" do
-    subject(:route_file) { file("config/routes.rb") }
-
-    it { is_expected.to exist }
-
-    it "has routes defined to the resource" do
-      expect(route_file).to contain("resources :users")
+      it { is_expected.to contain(/index/) }
+      it { is_expected.to exist }
     end
   end
 end
