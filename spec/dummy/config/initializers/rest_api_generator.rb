c6bac7fcc65e1cf48043ad6036a9e42476811e73
DavidsonGM <davidgmendes@hotmail.com>
2023-03-23 20:57:35 -0300

    :fire: remove useless comments

--- spec/dummy/config/initializers/rest_api_generator.rb
+++ spec/dummy/config/initializers/rest_api_generator.rb
@@ -1,7 +1,3 @@
-# config = RestApiGenerator::Config.new
-# config.test_path = "/test/requests"
-# config.docs_path = "/docs/specs"
-
 RestApiGenerator.configure do |config|
   config.test_path = "custom_test/requests"
   config.docs_path = "custom_docs/rswag"
