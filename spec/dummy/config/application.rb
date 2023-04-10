6193c358d1af9b338b2225a68a74a7e58ea494a0
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-04-10 17:39:03 -0300

    Fixes config example hosts in application.rb

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -26,6 +26,7 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << "www.example.com"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
